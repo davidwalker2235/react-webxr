7911083eb683e5fc2cf1d573b6c4e3c18f4bf431
David Carmona <davidwalker2235@gmail.com>
2022-10-05 16:34:12 +0200

    remove unnecessary code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,10 @@
 import Head from "next/head";
-import {
-  Twitch,
-  Twitter,
-  Linkedin,
-  Youtube,
-  Instagram,
-  GitHub,
-} from "react-feather";
 import Button from "../components/common/Button/Button";
-import HighScore from "../components/high-score/HighScore";
 import { useRouter } from "next/router";
 import MainLayout from "../components/layouts/MainLayout";
-import useVR from "../hooks/useVR";
 
 export default function Home() {
   const router = useRouter();
-  const { supported: vrSupport } = useVR();
   return (
     <div>
       <Head>
@@ -42,84 +31,11 @@ export default function Home() {
 
         <div className="margin-top">
           <Button
-            onClick={() => router.push(vrSupport ? "/game-3d" : "/game-2d")}
+            onClick={() => router.push("")}
             animate
           >
             Check simulator
           </Button>
-        </div>
-
-        <div className="margin-top">
-          <p>
-            Do you want to check out what our engineers (we call them ERNIans)
-            are up to? Check out our social media profiles.
-          </p>
-          <ul className="social-list">
-            <li>
-              <a
-                className="github"
-                href="https://github.com/ERNI-Academy"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <GitHub color="white" /> <span>Github</span>
-              </a>
-            </li>
-            <li>
-              <a
-                className="twitch"
-                href="https://www.twitch.tv/erni_academy"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Twitch color="#6441a5" /> <span>Twitch</span>
-              </a>
-            </li>
-            <li>
-              <a
-                className="youtube"
-                href="https://www.youtube.com/channel/UCkdDcxjml85-Ydn7Dc577WQ"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Youtube color="#ff0000" /> <span>Youtube</span>
-              </a>
-            </li>
-            <li>
-              <a
-                className="linkedin"
-                href="https://www.linkedin.com/company/erni/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Linkedin color="#0e76a8" /> <span>Linkedin</span>
-              </a>
-            </li>
-            <li>
-              <a
-                className="twitter"
-                href="https://twitter.com/ERNI"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Twitter color="#1da1f2" /> <span>Twitter</span>
-              </a>
-            </li>
-            <li>
-              <a
-                className="instagram"
-                href="https://www.instagram.com/ernigroup"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Instagram color="#c13584" /> <span>Instagram</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="margin-top">
-          <HighScore />
         </div>
       </MainLayout>
       <style jsx>{`
