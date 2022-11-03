d8c4592d91ccd345880d0ed869c41c6a0f092eeb
David Carmona - Contractor <David.Carmona@contractor.qiagen.com>
2022-11-03 18:08:37 +0100

    Create the game store

--- src/components/2d/Game2D.jsx
+++ src/components/2d/Game2D.jsx
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 import Button from "../common/Button/Button";
+import shallow from "zustand/shallow";
 import Intro2D from "./Intro2D";
+import {useGameStore} from "../../stores/gameStore";
 
 const Game2D = () => {
+  //Changes and detects if the game is running
+  const [isGameInit, gameInit] = useState(false);
   const [startButtonReady, setStartButtonReady] = useState(false);
 
-  return (
+  //Get the info from the store and re-render the component when any prop is changed
+  const {startGame} = useGameStore(
+    (state) => ({
+      startGame: state.startGame,
+    }),
+    shallow
+  );
+
+  //Init the game using the startGame() function from store
+  const initGame = () => {
+    gameInit(true);
+    startGame();
+  };
+
+  return !isGameInit ? (
     <div className="start-btn">
       <Intro2D onReady={() => setStartButtonReady(true)} />
       {startButtonReady && (
-        <Button animate onClick={() => null}>
+        <Button animate onClick={initGame}>
           START
         </Button>
       )}
@@ -20,7 +38,9 @@ const Game2D = () => {
         }
       `}</style>
     </div>
-  );
+    ) : (
+      <div>Game started!</div>
+    );
 };
 
 export default Game2D;
